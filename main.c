55d7bf41bdabbf7a8143634c5dc309a659ed1533
SebastianTylicki <115147343+SebastianTylicki@users.noreply.github.com>
2023-03-16 13:19:55 +0100

    Update main.c

--- main.c
+++ main.c
@@ -44,7 +44,7 @@ void zad212(int m4, int n4){
     double wynik = 0;
     double x = (1.0/m4);
     if(m4 <= 1){
-        printf("Podana liczba jest mniejsza równa 1\n ");
+        printf("Podana liczba jest mniejsza rÃ³wna 1\n ");
     }else{
         scanf("%d",&n4);
         for(int i = 0; i <= n4; i++){
@@ -82,5 +82,73 @@ int main()
     zad217(global);
     zad217(global);
 
+    return 0;
+}
+-------
+    
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void z21(){
+    printf("Zadanie 3.2.1\n");
+    int *n,*m;
+    scanf("%d %d",&n, &m);
+    printf("%d\n",zad21(&n,&m));
+}
+int zad21(int *n, int *m){
+    if(*n<=*m){
+        return *n;
+    }else{
+        return *m;
+    }
+}
+void z22(){
+    printf("Zadanie 3.2.2\n");
+    int *n,*m;
+    scanf("%d %d",&n, &m);
+    printf("%p\n",zad22(&n,&m));
+}
+int zad22(int*n, int*m){
+    if(*n<=*m){
+        return n;
+    }else{
+        return m;
+    }
+}
+void z24(){
+    printf("Zadanie 3.2.4\n");
+    int *n, *m;
+    scanf("%d %d",&n, &m);
+    printf("% \n",zad24(&n,&m));
+}
+void zad24(int*n, int*m){
+    int tmp;
+    if(*n>*m){
+        tmp = n;
+        n = m;
+        m = tmp;
+        printf("%d\n%d\n",*n,*m);
+    }else{
+        printf("%d\n%d\n",*n,*m);
+    }
+}
+void z26(){
+    printf("Zadanie 3.2.6\n");
+    int n, *w;
+    scanf("%d",&w);
+    printf("%d \n",zad26(n,&w));
+}
+void zad26(int n, int *w){
+    n = *w;
+    printf("%d\n%d\n",n,*w);
+}
+// 3.2.1 / 3.2.2 / 3.2.4 / 3.2.6 / 3.2.9 / 3.2.11 / 3.2.12 / 3.2.16
+int main()
+{
+    z21();
+    z22();
+    z24();
+    z26();
     return 0;
 }
